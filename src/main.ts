e1dcddd51cf73b91088395005af88cc66c29c7a0
leejh <leejh@helixtech.co.kr>
2024-01-16 16:13:19 +0900

    feat: gemini added

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,19 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
+    <h1>Chat With AI</h1>
     <p class="read-the-docs">
       This Is Daniel's LangChain Toy Project
     </p>
+    <div id='board'>
+    </div>
     <div class="card">
-      <button id="counter" type="button">
-        Start LLM
+      <input id='text' />
+      <button id="chat" type="button">
+        GO
       </button>
     </div>
   </div>
 `
 
-setLLM(document.querySelector<HTMLButtonElement>('#counter')!)
+setLLM(document.querySelector<HTMLButtonElement>('#chat')!)
--- src/setLLM.ts
+++ src/setLLM.ts
@@ -1,13 +1,51 @@
-import { ChatOpenAI } from '@langchain/openai'
+import { ChatGoogleGenerativeAI } from '@langchain/google-genai'
+import { HarmBlockThreshold, HarmCategory } from '@google/generative-ai'
+import { HumanMessage, MessageContent } from 'langchain/schema'
+import { marked } from 'marked'
 
-const chatModel = new ChatOpenAI({
-  openAIApiKey: import.meta.env.VITE_OPENAI_API_KEY,
+const model = new ChatGoogleGenerativeAI({
+  modelName: 'gemini-pro',
+  maxOutputTokens: 2048,
+  safetySettings: [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    },
+  ],
+  apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
 })
+
+const textChain: MessageContent = []
 
 export const setLLM = async (element: HTMLButtonElement) => {
   element.addEventListener('click', async () => {
-    const message = await chatModel.invoke('what is LangSmith?')
+    try {
+      const text = document.getElementById('text') as HTMLInputElement
 
-    console.log(message)
+      textChain.push({ type: 'text', text: text.value })
+
+      const input = [
+        new HumanMessage({
+          content: textChain,
+        }),
+      ]
+
+      const message = await model.invoke(input)
+
+      if (message && document) {
+        const board = document.getElementById('board')
+        const md = await marked(message.content as string)
+
+        if (md) {
+          const body = document.createElement('div')
+
+          body.innerHTML = md
+
+          board && md && board.appendChild(body)
+        }
+      }
+    } catch (e) {
+      console.error(e)
+    }
   })
 }
